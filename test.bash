eaa90f1e751b4eb7ed77a9e1402da26d92157615
Jack Palevich <jack.palevich@gmail.com>
2010-01-21 15:41:02 +0800

    Remove Ubuntu test torrent. It won't be valid forever.

--- test.bash
+++ test.bash
@@ -1,4 +1,5 @@
 #!/bin/bash
 # Test the torrent
 
-./taipeitorrent -torrent=testData/ubuntu-9.10-desktop-amd64.iso.torrent -fileDir=testData/downloads -port 6881 -useUPnP true
+# ./taipeitorrent -torrent=testData/a.torrent -fileDir=testData/downloads -port 6881 -useUPnP true
+./taipeitorrent -torrent=testData/a.torrent -fileDir=testData/downloads -port 6881
