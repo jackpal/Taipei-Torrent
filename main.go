e6bca305f4548e5ff165cb4981fabe7aa98f2b50
Jack Palevich <jack.palevich@gmail.com>
2010-07-12 20:41:09 -0700

    Various changes...

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 )
 
 var torrent *string = flag.String("torrent", "", "URL or path to a torrent file")
-var fileDir *string = flag.String("fileDir", "", "path to directory where files are stored")
+var fileDir *string = flag.String("fileDir", ".", "path to directory where files are stored")
 var debugp *bool = flag.Bool("debug", false, "Turn on debugging")
 var port *int = flag.Int("port", 0, "Port to listen on. Defaults to random.")
 var useUPnP *bool = flag.Bool("useUPnP", false, "Use UPnP to open port in firewall.")
