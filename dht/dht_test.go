224209b4ddb30fa5233dc0eb8470bae4278db903
Yves Junqueira <yves.junqueira@gmail.com>
2012-03-09 00:38:13 +0100

    Renaming test files

--- dht/dht_test.go
+++ dht/dht_test.go
@@ -1,159 +1,93 @@
 package dht
 
 import (
-	"crypto/rand"
-	"fmt"
-	"log"
-	"sort"
-	"strings"
+	"math/rand"
+	"net"
 	"testing"
+	"time"
+
+	"github.com/nictuku/Taipei-Torrent/bencode"
 )
 
-func BenchmarkFindClosest(b *testing.B) {
-	b.StopTimer()
-
-	// 16 bytes prefix for very distant nodes.
-	distant := "\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff"
-
-	node, err := NewDhtNode("00bcdefghij01234567", 0, 1e7)
+func startDhtNode(t *testing.T) *DhtEngine {
+	port := rand.Intn(10000) + 40000
+	node, err := NewDhtNode("abcdefghij0123456789", port, 100)
 	if err != nil {
-		log.Fatal(err)
+		t.Errorf("NewDhtNode(): %v", err)
 	}
-
-	// Add 100k nodes to the remote nodes slice.
-	for i := 0; i < 100000; i++ {
-
-		rId := make([]byte, 4)
-		if _, err := rand.Read(rId); err != nil {
-			log.Fatal("Couldnt produce random numbers for FindClosest:", err)
-		}
-		r, _ := node.newRemoteNode(distant+string(rId), ":0")
-		if len(r.id) != 20 {
-			log.Fatalf("DhtRemoteNode construction error, wrong len: want %d, got %d",
-				20, len(r.id))
-		}
-		r.reachable = true
-	}
-	b.StartTimer()
-	for i := 0; i < b.N; i++ {
-		f := node.tree.lookupFiltered(fmt.Sprintf("x%10v", i) + "xxxxxxxxx")
-		if len(f) != kNodes {
-			log.Fatalf("Missing results. Wanted %d, got %d", kNodes, len(f))
-		}
-	}
+	go node.DoDht()
+	return node
 }
 
-type testData struct {
-	query string
-	want  int // just the size.
+func dumpStats() {
 }
 
-var nodes = []*DhtRemoteNode{
-	{id: "\x00"},
-	{id: "\x01"},
-	{id: "\x02"},
-	{id: "\x03"},
-	{id: "\x04"},
-	{id: "\x05"},
-	{id: "\x06"},
-	{id: "\x07"},
-	{id: "\x08"},
-	{id: "\x09"},
-	{id: "\x10"},
+// Requires Internet access and can be flaky if the server or the internet is
+// slow.
+func TestDhtLarge(t *testing.T) {
+	node := startDhtNode(t)
+	realDHTNodes := []string{
+		"1.a.magnets.im",
+	}
+	for _, addr := range realDHTNodes {
+		ip, err := net.LookupHost(addr)
+		if err != nil {
+			t.Error(err)
+			continue
+		}
+		node.Ping(ip[0] + ":6881")
+	}
+
+	// Test that we can reach at least one node.
+	success := false
+	for i := 0; i < 10; i++ {
+		tbl := node.RoutingTable()
+		if len(tbl) > 0 {
+			t.Logf("Contacted %d DHT nodes.", len(tbl))
+			success = true
+			break
+		}
+		time.Sleep(time.Second)
+	}
+	if !success {
+		t.Fatal("No external DHT node could be contacted.")
+	}
+
+	// Test that we can find peers for a known torrent in a timely fashion.
+	//
+	// Torrent from: http://www.clearbits.net/torrents/244-time-management-for-anarchists-1
+	infoHash := "\xb4\x62\xc0\xa8\xbc\xef\x1c\xe5\xbb\x56\xb9\xfd\xb8\xcf\x37\xff\xd0\x2f\x5f\x59"
+	go node.PeersRequest(infoHash, true)
+	timeout := make(chan bool, 1)
+	go func() {
+		time.Sleep(10 * time.Second)
+		timeout <- true
+	}()
+	var infoHashPeers map[string][]string
+	select {
+	case infoHashPeers = <-node.PeersRequestResults:
+		t.Logf("Found %d peers.", len(infoHashPeers[infoHash]))
+	case <-timeout:
+		t.Fatal("Could not find new peers: timed out")
+	}
+	for ih, peers := range infoHashPeers {
+		if infoHash != ih {
+			t.Fatal("Unexpected infohash returned")
+		}
+		if len(peers) == 0 {
+			t.Fatal("Could not find new torrent peers.")
+		}
+		for _, peer := range peers {
+			t.Logf("peer found: %+v\n", bencode.BinaryToDottedPort(peer))
+		}
+	}
+	t.Logf("=== Stats ===")
+	t.Logf("totalReachableNodes: %v", totalReachableNodes)
+	t.Logf("totalDupes: %v", totalDupes)
+	t.Logf("totalPeers: %v", totalPeers)
+	t.Logf("totalSentGetPeers: %v", totalSentGetPeers)
 }
 
-func TestNodeDelete(t *testing.T) {
-	tree := &nTree{}
-
-	for _, r := range nodes[:4] {
-		tree.insert(r)
-	}
-	for i, r := range []string{"\x00", "\x01"} {
-		// t.Logf("cutting: %x", r)
-		tree.cut(r, 0)
-		neighbors := tree.lookup(r)
-		if len(neighbors) == 0 {
-			t.Errorf("Deleted too many nodes.")
-		}
-		if len(neighbors) != 3-i {
-			t.Errorf("Too many nodes left in the tree: got %d, wanted %d", len(neighbors), 3-i)
-		}
-		if r == neighbors[0].id {
-			t.Errorf("Node didnt get deleted as expected: %x", r)
-		}
-		// for _, x := range neighbors {
-		// 	t.Logf("%x", x.id)
-		// }
-	}
-
+func init() {
+	rand.Seed((time.Now().Unix() % (1e9 - 1)))
 }
-
-func TestNodeDistance(t *testing.T) {
-
-	tree := &nTree{}
-
-	for _, r := range nodes {
-		r.reachable = true
-		tree.insert(r)
-	}
-
-	tests := []testData{
-		{"\x04", 8},
-		{"\x07", 8},
-	}
-
-	for _, r := range tests {
-		distances := make([]string, 0, len(tests))
-		neighbors := tree.lookup(r.query)
-		if len(neighbors) != r.want {
-			t.Errorf("id: %x, wanted len=%d, got len=%d", r.query, r.want, len(neighbors))
-			t.Errorf("Details: %#v", neighbors)
-		}
-
-		for _, x := range neighbors {
-			d := hashDistance(r.query, x.id)
-			var b []string
-			for _, c := range d {
-				if c != 0 {
-					b = append(b, fmt.Sprintf("%08b", c))
-				} else {
-					b = append(b, "00000000")
-				}
-			}
-			d = strings.Join(b, ".")
-			distances = append(distances, d)
-		}
-		if !sort.StringsAreSorted(distances) {
-			t.Errorf("Resulting distances for %x are not sorted", r.query)
-			for i, d := range distances {
-				t.Errorf("id: %x, d: %v", neighbors[i].id, d)
-			}
-		}
-	}
-
-}
-
-// $ go test -v -bench='BenchmarkFindClosest' -run=NONE
-//
-// Results for nictuku's machine.
-//
-// #1
-// BenchmarkFindClosest	       1	7020661000 ns/op
-//
-// #2 not-checked in attempt to use a trie. Not even correct.
-// BenchmarkFindClosest	       1	1072682000 ns/op
-//
-// #3 only compare bytes that we need. 
-// BenchmarkFindClosest	       1	1116333000 ns/op
-//
-// #4 moved to buckets, but using only one.
-// BenchmarkFindClosest	       1	1170809000 ns/op
-//
-// #5 using my new nTree (not yet correct)
-// BenchmarkFindClosest	  100000	     27194 ns/op
-//
-// #6 recursive nTree (correct)
-// BenchmarkFindClosest	  200000	     10585 ns/op
-//
-// #7 removed an unnecessary wrapper function.
-// BenchmarkFindClosest	  200000	      9691 ns/op
---
+++ dht/routing_test.go
@@ -0,0 +1,159 @@
+package dht
+
+import (
+	"crypto/rand"
+	"fmt"
+	"log"
+	"sort"
+	"strings"
+	"testing"
+)
+
+func BenchmarkFindClosest(b *testing.B) {
+	b.StopTimer()
+
+	// 16 bytes prefix for very distant nodes.
+	distant := "\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff"
+
+	node, err := NewDhtNode("00bcdefghij01234567", 0, 1e7)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Add 100k nodes to the remote nodes slice.
+	for i := 0; i < 100000; i++ {
+
+		rId := make([]byte, 4)
+		if _, err := rand.Read(rId); err != nil {
+			log.Fatal("Couldnt produce random numbers for FindClosest:", err)
+		}
+		r, _ := node.newRemoteNode(distant+string(rId), ":0")
+		if len(r.id) != 20 {
+			log.Fatalf("DhtRemoteNode construction error, wrong len: want %d, got %d",
+				20, len(r.id))
+		}
+		r.reachable = true
+	}
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		f := node.tree.lookupFiltered(fmt.Sprintf("x%10v", i) + "xxxxxxxxx")
+		if len(f) != kNodes {
+			log.Fatalf("Missing results. Wanted %d, got %d", kNodes, len(f))
+		}
+	}
+}
+
+type testData struct {
+	query string
+	want  int // just the size.
+}
+
+var nodes = []*DhtRemoteNode{
+	{id: "\x00"},
+	{id: "\x01"},
+	{id: "\x02"},
+	{id: "\x03"},
+	{id: "\x04"},
+	{id: "\x05"},
+	{id: "\x06"},
+	{id: "\x07"},
+	{id: "\x08"},
+	{id: "\x09"},
+	{id: "\x10"},
+}
+
+func TestNodeDelete(t *testing.T) {
+	tree := &nTree{}
+
+	for _, r := range nodes[:4] {
+		tree.insert(r)
+	}
+	for i, r := range []string{"\x00", "\x01"} {
+		// t.Logf("cutting: %x", r)
+		tree.cut(r, 0)
+		neighbors := tree.lookup(r)
+		if len(neighbors) == 0 {
+			t.Errorf("Deleted too many nodes.")
+		}
+		if len(neighbors) != 3-i {
+			t.Errorf("Too many nodes left in the tree: got %d, wanted %d", len(neighbors), 3-i)
+		}
+		if r == neighbors[0].id {
+			t.Errorf("Node didnt get deleted as expected: %x", r)
+		}
+		// for _, x := range neighbors {
+		// 	t.Logf("%x", x.id)
+		// }
+	}
+
+}
+
+func TestNodeDistance(t *testing.T) {
+
+	tree := &nTree{}
+
+	for _, r := range nodes {
+		r.reachable = true
+		tree.insert(r)
+	}
+
+	tests := []testData{
+		{"\x04", 8},
+		{"\x07", 8},
+	}
+
+	for _, r := range tests {
+		distances := make([]string, 0, len(tests))
+		neighbors := tree.lookup(r.query)
+		if len(neighbors) != r.want {
+			t.Errorf("id: %x, wanted len=%d, got len=%d", r.query, r.want, len(neighbors))
+			t.Errorf("Details: %#v", neighbors)
+		}
+
+		for _, x := range neighbors {
+			d := hashDistance(r.query, x.id)
+			var b []string
+			for _, c := range d {
+				if c != 0 {
+					b = append(b, fmt.Sprintf("%08b", c))
+				} else {
+					b = append(b, "00000000")
+				}
+			}
+			d = strings.Join(b, ".")
+			distances = append(distances, d)
+		}
+		if !sort.StringsAreSorted(distances) {
+			t.Errorf("Resulting distances for %x are not sorted", r.query)
+			for i, d := range distances {
+				t.Errorf("id: %x, d: %v", neighbors[i].id, d)
+			}
+		}
+	}
+
+}
+
+// $ go test -v -bench='BenchmarkFindClosest' -run=NONE
+//
+// Results for nictuku's machine.
+//
+// #1
+// BenchmarkFindClosest	       1	7020661000 ns/op
+//
+// #2 not-checked in attempt to use a trie. Not even correct.
+// BenchmarkFindClosest	       1	1072682000 ns/op
+//
+// #3 only compare bytes that we need. 
+// BenchmarkFindClosest	       1	1116333000 ns/op
+//
+// #4 moved to buckets, but using only one.
+// BenchmarkFindClosest	       1	1170809000 ns/op
+//
+// #5 using my new nTree (not yet correct)
+// BenchmarkFindClosest	  100000	     27194 ns/op
+//
+// #6 recursive nTree (correct)
+// BenchmarkFindClosest	  200000	     10585 ns/op
+//
+// #7 removed an unnecessary wrapper function.
+// BenchmarkFindClosest	  200000	      9691 ns/op
