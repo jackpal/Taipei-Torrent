d76b5bea04fd2964448979308c622166c605f8dd
Yves Junqueira <yves.junqueira@gmail.com>
2012-07-08 13:54:32 +0200

    Parse magnet links.

--- taipei/metainfo.go
+++ taipei/metainfo.go
@@ -55,12 +55,16 @@ func getString(m map[string]interface{}, k string) string {
 func getMetaInfo(torrent string) (metaInfo *MetaInfo, err error) {
 	var input io.ReadCloser
 	if strings.HasPrefix(torrent, "http:") {
-		// 6g compiler bug prevents us from writing r, _, err :=
-		var r *http.Response
-		if r, err = http.Get(torrent); err != nil {
-			return
+		r, err := http.Get(torrent)
+		if err != nil {
+			return nil, err
 		}
 		input = r.Body
+	} else if strings.HasPrefix(torrent, "magnet:") {
+		input, err = torrentFromMagnet(torrent)
+		if err != nil {
+			return
+		}
 	} else {
 		if input, err = os.Open(torrent); err != nil {
 			return
