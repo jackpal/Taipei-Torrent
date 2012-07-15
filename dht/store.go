ad86a559d90ee11455425c64b65e7c2a3b085481
Yves Junqueira <yves.junqueira@gmail.com>
2012-07-15 15:24:11 +0200

    DHT config store, Win support

--- dht/store.go
+++ dht/store.go
@@ -6,6 +6,7 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
+	"path"
 	"strings"
 )
 
@@ -17,18 +18,18 @@ func init() {
 
 // mkdirStore() creates a directory to load and save the configuration from.
 // Uses ~/.taipeitorrent if $HOME is set, otherwise falls back to
-// /var/run/taipeitorrent. It's very much UNIX only at the moment, but making
-// it generic should be fairly easy.
+// /var/run/taipeitorrent.
 func mkdirStore() string {
 	dir := "/var/run/taipeitorrent"
 	env := os.Environ()
 	for _, e := range env {
 		if strings.HasPrefix(e, "HOME=") {
-			dir = strings.SplitN(e, "=", 2)[1] + "/.taipeitorrent"
+			dir = strings.SplitN(e, "=", 2)[1]
+			dir = path.Join(dir, ".taipeitorrent")
 		}
 	}
 	// Ignore errors.
-	os.Mkdir(dir, 0750)
+	os.MkdirAll(dir, 0750)
 
 	if s, err := os.Stat(dir); err != nil {
 		log.Fatal("stat config dir", err)
@@ -51,7 +52,7 @@ func openStore(port int) (cfg *DHTStore) {
 
 	// If a node is running in port 30610, the config should be in
 	// ~/.taipeitorrent/dht-36010
-	p := fmt.Sprintf("%v-%v", configFilePrefix+"/dht", port)
+	p := fmt.Sprintf("%v-%v", path.Join(configFilePrefix, "dht"), port)
 	f, err := os.Open(p)
 	if err != nil {
 		// log.Println(err)
@@ -73,18 +74,39 @@ func saveStore(s DHTStore) {
 		log.Println("saveStore tempfile:", err)
 		return
 	}
-	defer tmp.Close()
-
-	enc := json.NewEncoder(tmp)
-	if err = enc.Encode(s); err != nil {
+	err = json.NewEncoder(tmp).Encode(s)
+	// The file has to be closed already otherwise it can't be renamed on
+	// Windows.
+	tmp.Close()
+	if err != nil {
 		log.Println("saveStore json encoding:", err)
 		return
 	}
 
-	// Write worked, so now we can replace the existing file, which is atomic AFAIK.
+	// Write worked, so replace the existing file. That's atomic in Linux, but
+	// not on Windows.
 	p := fmt.Sprintf("%v-%v", configFilePrefix+"/dht", s.Port)
 	if err := os.Rename(tmp.Name(), p); err != nil {
-		log.Println("saveStore failed when replacing existing config:", err)
+		// if os.IsExist(err) {
+		// Not working for Windows:
+		// http://code.google.com/p/go/issues/detail?id=3828
+
+		// It's not possible to atomically rename files on Windows, so I
+		// have to delete it and try again. If the program crashes between
+		// the unlink and the rename operation, it lose the configuration,
+		// unfortunately.
+		if err := os.Remove(p); err != nil {
+			log.Println("saveStore failed to remove the existing config:", err)
+			return
+		}
+		if err := os.Rename(tmp.Name(), p); err != nil {
+			log.Println("saveStore failed to rename file after deleting the original config:", err)
+			return
+		}
+		// } else {
+		// 	log.Println("saveStore failed when replacing existing config:", err)
+		// }
+	} else {
+		log.Println("Saved DHT routing table to the filesystem.")
 	}
-	log.Println("Saved DHT routing table to the filesystem.")
 }
