735b63ac97af30f22ce96ceb3b316b76625327e2
Yves Junqueira <yves.junqueira@gmail.com>
2012-09-06 02:31:19 +0200

    Update tests to new command line syntax

--- testdht.bash
+++ testdht.bash
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Test the torrent
 
-./Taipei-Torrent -torrent=testData/a.torrent -fileDir=testData/downloads -port 63881 -useUPnP=false -useDHT -trackerLessMode=true
+./Taipei-Torrent -fileDir=testData/downloads -port 63881 -useUPnP=false -useDHT -trackerLessMode=true testData/a.torrent
