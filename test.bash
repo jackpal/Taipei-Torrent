d4e55f4e22ab37b9b8ec2eb3321f0a0bbf44c3ca
Jack Palevich <jack.palevich@gmail.com>
2014-05-04 07:16:10 -0700

    Run unit tests as part of torrent test.

--- test.bash
+++ test.bash
@@ -1,10 +1,14 @@
 #!/bin/bash
 # Test the torrent
 set -e
-
+echo "Building..."
 go clean
 go build -race
-
+echo "Running unit tests..."
+cd torrent
+go test -race
+cd ..
+echo "Running a torrent. Type control-C to exit"
 # ./Taipei-Torrent -fileDir=testData/downloads -port=63881 -useUPnP=true testData/a.torrent
 # ./Taipei-Torrent -fileDir=testData/downloads -port=63881 -useNATPMP -gateway 192.168.1.1 testData/a.torrent
 ./Taipei-Torrent -fileDir=testData/downloads -port=63881 testData/a.torrent
